84c9a281dccf86d9a3aed8c7972f1cbb1668d64e
skuba <>
2023-08-08 15:54:15 +0800

    Clone greeter

---
+++ skuba.template.js
@@ -0,0 +1,16 @@
+/**
+ * Run `skuba configure` to finish templating and remove this file.
+ */
+
+module.exports = {
+  entryPoint: 'src/app.ts',
+  fields: [
+    {
+      name: 'prodBuildkiteQueueName',
+      message: 'Prod Buildkite queue',
+      initial: 'my-team-aws-account-prod:cicd',
+      validate: (value) => /^.+:.+$/.test(value),
+    },
+  ],
+  type: 'application',
+};
